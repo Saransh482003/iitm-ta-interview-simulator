dfea692d6ad4aa3b3c77ea101509c3786d3ed095
Saransh482003 <saini.saransh03@gmail.com>
2025-09-04 13:27:39 +0530

    Netlify TOML added

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",   // 👈 tells Next.js to do static export
 };
 
 export default nextConfig;
